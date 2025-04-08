f54c9a80c83f70130d8c465398bd774cc8c2f853
sang53 <143582855+sang53@users.noreply.github.com>
2025-04-08 11:35:01 +1000

    add citizen status

--- src/Main/Contact/Contact.tsx
+++ src/Main/Contact/Contact.tsx
@@ -8,6 +8,7 @@ export default function Resume() {
         <span>sang1cho8912@gmail.com</span>
         <span>0403 727 403</span>
         <span>Wakeley, NSW 2176</span>
+        <span>Australian Citizen</span>
       </div>
       <a href="/resume/Resume.pdf" target="_blank" rel="noreferrer">
         Resume PDF
